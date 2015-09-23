005cecc7098c1a4a42b4fe97cc08545b44b10e9e
Elihu Cruz <ea_2305@hotmail.com>
2015-09-22 23:55:42 -0500

    Funciones de PyQt agregadas junto con archivo .bat

---
+++ programa_prueba.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'programa_prueba.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(419, 364)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.lineEdit = QtGui.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(20, 290, 271, 21))
+        self.lineEdit.setText(_fromUtf8(""))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(320, 290, 75, 23))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.label = QtGui.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(90, 200, 241, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.setObjectName(_fromUtf8("label"))
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 419, 21))
+        self.menubar.setObjectName(_fromUtf8("menubar"))
+        self.menuSettings = QtGui.QMenu(self.menubar)
+        self.menuSettings.setObjectName(_fromUtf8("menuSettings"))
+        self.menuInformation = QtGui.QMenu(self.menuSettings)
+        self.menuInformation.setObjectName(_fromUtf8("menuInformation"))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionStaff = QtGui.QAction(MainWindow)
+        self.actionStaff.setObjectName(_fromUtf8("actionStaff"))
+        self.menuInformation.addAction(self.actionStaff)
+        self.menuSettings.addAction(self.menuInformation.menuAction())
+        self.menubar.addAction(self.menuSettings.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.pushButton.setText(_translate("MainWindow", "Click me!", None))
+        self.label.setText(_translate("MainWindow", "Introduzca su nombre", None))
+        self.menuSettings.setTitle(_translate("MainWindow", "Settings", None))
+        self.menuInformation.setTitle(_translate("MainWindow", "Information", None))
+        self.actionStaff.setText(_translate("MainWindow", "Staff", None))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    MainWindow = QtGui.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
